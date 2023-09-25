c00f6c7b1d877ee639b9df9f10b4ef97295ee57c
Anandu Babu <anandubabu.sd@gmail.com>
2023-09-25 16:36:49 +0530

    feat - added basic ui

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vo77iovubmg"
+    "revision": "0.3lblm4r3ft"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 function App() {
-  const [todoes, setTodoes] = useState([]);
+  const [todoes, setTodoes] = useState([
+    { text: "Todo text", isComplete: false },
+    { text: "Todo text 2", isComplete: false },
+  ]);
 
   return (
     <div className="p-6">
-      <h1 className="text-xl text-center my-4">Todo</h1>
+      <h1 className="text-xl text-center my-4">My Todos</h1>
       <main className="flex flex-col">
         <form className="flex mx-auto max-w-xl rounded-lg overflow-hidden border border-slate-200 w-full justify-between">
           <input className="p-2 outline-none w-full" type="text" />
@@ -14,17 +17,23 @@ function App() {
           </button>
         </form>
 
-        <div className="mt-5">
-          <div className="flex mx-auto items-center rounded-xl overflow-hidden w-full max-w-xl bg-slate-100 justify-between">
-            <p className="p-2 ">todo item</p>
-            <div>
-              <button className="p-2 bg-red-500 rounded-full ml-2">co</button>
-              <button className="p-2 bg-green-500 rounded-full ml-2">do</button>
-              <button className="p-2 bg-orange-500 rounded-full ml-2">
-                rm
-              </button>
+        <hr className="my-5 w-1/2 mx-auto" />
+
+        <div>
+          {todoes.map((todoItem) => (
+            <div className="flex mx-auto m-2 items-center rounded-xl overflow-hidden w-full max-w-xl bg-slate-100 justify-between">
+              <p className="p-2 ">{todoItem.text}</p>
+              <div>
+                <button className="p-2 bg-red-500 rounded-full ml-2">co</button>
+                <button className="p-2 bg-green-500 rounded-full ml-2">
+                  do
+                </button>
+                <button className="p-2 bg-orange-500 rounded-full ml-2">
+                  rm
+                </button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </main>
     </div>
