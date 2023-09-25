0c8760fb95f3f250bb8a8b436e32413cae825942
Anandu Babu <anandubabu.sd@gmail.com>
2023-09-25 14:38:07 +0530

    config - added pwa configuration with manifesto

---
+++ manifesto.js
@@ -0,0 +1,35 @@
+export const manifesto = {
+  name: "todo PWA by AnB",
+  short_name: "todo PWA",
+  description: "An app that for todo .",
+  icons: [
+    {
+      src: "vite.svg",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "vite.svg",
+      sizes: "512x512",
+      type: "image/png",
+    },
+    {
+      src: "vite.svg",
+      sizes: "180x180",
+      type: "image/png",
+      purpose: "apple touch icon",
+    },
+    {
+      src: "vite.svg",
+      sizes: "225x225",
+      type: "image/png",
+      purpose: "any maskable",
+    },
+  ],
+  theme_color: "#1a1a1a",
+  background_color: "#1a1a1a",
+  display: "standalone",
+  scope: "/",
+  start_url: "/",
+  orientation: "landscape",
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
-
+import { manifesto } from './manifesto.js'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    registerType: 'autoUpdate', devOptions: {
+    registerType: 'autoUpdate', manifest: { ...manifesto }, devOptions: {
       enabled: true
     }
   },)],
