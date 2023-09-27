fa8401c705ba076240773973e30310cc256f0c5c
Anandu Babu <anandubabu.sd@gmail.com>
2023-09-27 12:00:46 +0530

    feat add controlled from #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function App() {
+  const [taskName, setTaskName] = useState("");
+
   const todoes = [
     { text: "Todo text", isComplete: false },
     { text: "Todo text 2", isComplete: false },
@@ -9,7 +13,12 @@ function App() {
       <h1 className="text-xl text-center my-4">My Todos</h1>
       <main className="flex flex-col">
         <form className="flex mx-auto max-w-xl rounded-lg overflow-hidden border border-slate-200 w-full justify-between">
-          <input className="p-2 outline-none w-full" type="text" />
+          <input
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+            className="p-2 outline-none w-full"
+            type="text"
+          />
           <button className="p-2 bg-orange-500 w-24 text-white font-black text-2xl">
             +
           </button>
@@ -22,7 +31,7 @@ function App() {
             <div className="flex mx-auto m-2 items-center rounded-xl overflow-hidden w-full max-w-xl bg-slate-100 justify-between">
               <p className="p-2 ">{todoItem.text}</p>
               <div>
-                <button className="p-2 bg-red-500 rounded-full ml-2">co</button>
+                <button className="p-2 bg-red-500 rounded-full ml-2">de</button>
                 <button className="p-2 bg-green-500 rounded-full ml-2">
                   do
                 </button>
