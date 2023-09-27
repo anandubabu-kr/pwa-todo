888316b95c28af720d302fd2de5fdb24b20ff616
Anandu Babu <anandubabu.sd@gmail.com>
2023-09-27 14:28:44 +0530

    refactor - add Form Component #7

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
+import AddForm from "./components/AddForm";
 import TaskItem from "./components/TaskItem";
 import { todoItemType } from "./types";
 
 function App() {
-  const [taskName, setTaskName] = useState("");
-
   const todoes = [
     { text: "Todo text", isComplete: false },
     { text: "Todo text 2", isComplete: false },
@@ -12,19 +10,9 @@ function App() {
 
   return (
     <div className="p-6">
-      <h1 className="text-xl text-center my-4">My Todos</h1>
+      <h1 className="text-xl text-center my-4">My Tasks</h1>
       <main className="flex flex-col">
-        <form className="flex mx-auto max-w-xl rounded-lg overflow-hidden border border-slate-200 w-full justify-between">
-          <input
-            value={taskName}
-            onChange={(e) => setTaskName(e.target.value)}
-            className="p-2 outline-none w-full"
-            type="text"
-          />
-          <button className="p-2 bg-orange-500 w-24 text-white font-black text-2xl">
-            +
-          </button>
-        </form>
+        <AddForm />
 
         <hr className="my-5 w-1/2 mx-auto" />
 
--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const AddForm = () => {
-  return <div>AddForm</div>;
+  const [taskName, setTaskName] = useState("");
+
+  return (
+    <form className="flex mx-auto max-w-xl rounded-lg overflow-hidden border border-slate-200 w-full justify-between">
+      <input
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+        className="p-2 outline-none w-full"
+        type="text"
+      />
+      <button className="p-2 bg-orange-500 w-24 text-white font-black text-2xl">
+        +
+      </button>
+    </form>
+  );
 };
 
 export default AddForm;
