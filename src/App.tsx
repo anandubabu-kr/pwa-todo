2df5e74bd9397e257165ed2cb7578e02538dd105
Anandu Babu <anandubabu.sd@gmail.com>
2023-10-04 14:07:34 +0530

    feat - added local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddForm from "./components/AddForm";
 import TaskItem from "./components/TaskItem";
 import { todoItemType } from "./types";
 
+const key = "task";
+
+const getStoredItems = () => {
+  return JSON.parse(localStorage.getItem(key) || "[]");
+};
+
 function App() {
-  const [todoes, setTodoes] = useState<todoItemType[]>([]);
+  const [todos, setTodos] = useState<todoItemType[]>(getStoredItems());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(todos));
+  }, [todos]);
 
   const addNewItem = (item: todoItemType) => {
-    setTodoes([...todoes, item]);
+    setTodos([...todos, item]);
   };
 
   const deleteItem = (index: number) => {
-    const newList = todoes.filter((_item, i) => i !== index);
+    const newList = todos.filter((_item, i) => i !== index);
 
-    setTodoes(newList);
+    setTodos(newList);
   };
 
   const completeTask = (index: number) => {
-    const updatedItem: todoItemType = { ...todoes[index], isComplete: true };
-    const newArray = [...todoes];
+    const updatedItem: todoItemType = { ...todos[index], isComplete: true };
+    const newArray = [...todos];
 
     newArray[index] = updatedItem;
-    setTodoes(newArray);
+    setTodos(newArray);
   };
 
   return (
@@ -35,10 +45,10 @@ function App() {
       <main className="flex flex-col px-3">
         {/* <hr className="my-2 w-1/2 mx-auto" /> */}
         <div className="h-[75vh] md:h-[75vh] overflow-y-auto mx-auto p-2 inner-shadow max-w-2xl w-full rounded-xl">
-          {!todoes.length && (
+          {!todos.length && (
             <p className="text-center text-slate-500">No items yet!!</p>
           )}
-          {todoes.map((todoItem: todoItemType, index: number) => (
+          {todos.map((todoItem: todoItemType, index: number) => (
             <TaskItem
               key={index}
               index={index}
@@ -52,7 +62,7 @@ function App() {
         <AddForm addNewItem={addNewItem} />
         <a
           className="text-center text-orange-600"
-          href="http://anandubabu.netlify.app"
+          href="http://anandubabu.web.app"
           target="_blank"
           rel="noopener noreferrer"
         >
